f92e1575449977fd60bf0eaeb9ade9049544fe3b
Aleksandr <104426761+nemirovka28@users.noreply.github.com>
2022-07-13 23:14:00 +0300

    add fetch countries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import './sass/index.scss'
+import './sass/index.scss';
+import APi from './js/fetchCountries.js'
+import countries from './js/countries.json';
+
+const galleryRef = document.querySelector('ul');
+function onSearch (event) {
+    e.preventDefault();
+
+    APi.fetchCountries(searchQuery)
+    .then(renderCountry)
+
+}
+function renderCountry (country) {
+    console.log(country)
+}
+
+// const country = APi.fetchCountries('peru').then( c => console.log(c))
+// console.log(country)
+
+
+// galleryRef.insertAdjacentHTML('beforeend', createCountry(countries));
